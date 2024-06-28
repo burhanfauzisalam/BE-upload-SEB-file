ba1acc8a22ff46918f78a63456e337a107b16196
= <=>
2024-06-28 17:00:11 +0700

    modify(input report value)

--- routes/reportCardRouter.js
+++ routes/reportCardRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 //
 router.post("/input", inputValue.addData);
-router.get("/data", inputValue.getData);
+router.post("/data", inputValue.getData);
 router.get("/data-input", inputValue.listData);
 router.post("/add-rubric", inputValue.addRubric);
 
